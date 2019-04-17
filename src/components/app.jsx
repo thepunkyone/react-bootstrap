376217d27657600e0da1069455723be48fbecc6f
Vitnija Bluzma <thepunkyone@gmail.com>
2019-04-17 22:33:22 +0100

    added PropTypes to LocationDetails component, updated styling according to the walkthrough

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import LocationDetails from '../components/location-details';
 
-const App = props => {
-  return <LocationDetails city={props.location.city} country={props.location.country} />;
-};
-
+const App = props => (
+  <LocationDetails
+    city={props.location.city}
+    country={props.location.country}
+  />
+);
 
 App.propTypes = {
   location: PropTypes.shape({
     city: PropTypes.string,
     country: PropTypes.string,
-  }),
+  }).isRequired,
 };
 
 export default App;
