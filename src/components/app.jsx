feaecbc050d9deab8ef58587473f7b171aa04362
Vitnija Bluzma <thepunkyone@gmail.com>
2019-04-17 22:09:53 +0100

    set up project and created location-details module

---
+++ src/components/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import LocationDetails from '../components/location-details';
+
+const App = props => {
+  return <LocationDetails city={props.location.city} country={props.location.country} />;
+};
+
+
+App.propTypes = {
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }),
+};
+
+export default App;
---
+++ src/components/location-details.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const LocationDetails = props => <h1 className="location-details">{props.city}, {props.country}</h1>;
+
+export default LocationDetails;
