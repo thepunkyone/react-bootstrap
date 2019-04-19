83d7b533393f3546400ae017cd7fc5072e8454cf
Vitnija Bluzma <thepunkyone@gmail.com>
2019-04-19 14:20:08 +0100

    created ForecastSummaries component and test file

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import LocationDetails from '../components/location-details';
+import ForecastSummaries from '../components/forecast-summaries';
 
 const App = props => (
-  <LocationDetails
-    city={props.location.city}
-    country={props.location.country}
-  />
+  <div className="forecast">
+    <LocationDetails
+      city={props.location.city}
+      country={props.location.country}
+    />
+    <ForecastSummaries forecasts={props.forecasts} />
+  </div>
 );
 
 App.propTypes = {
@@ -14,6 +18,7 @@ App.propTypes = {
     city: PropTypes.string,
     country: PropTypes.string,
   }).isRequired,
+  forecasts: PropTypes.array.isRequired,
 };
 
 export default App;
