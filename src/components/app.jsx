79cf61726c2a9dc402ecfb9042096f952b43000b
Vitnija Bluzma <thepunkyone@gmail.com>
2019-04-22 20:38:56 +0100

    added ForecastDetails component and rendered it in App component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import LocationDetails from '../components/location-details';
 import ForecastSummaries from '../components/forecast-summaries';
+import ForecastDetails from '../components/forecast-details';
 import '../styles/app.scss';
 
 const App = props => (
@@ -11,6 +12,7 @@ const App = props => (
       country={props.location.country}
     />
     <ForecastSummaries forecasts={props.forecasts} />
+    <ForecastDetails forecast={props.forecasts[0]} />
   </div>
 );
 
