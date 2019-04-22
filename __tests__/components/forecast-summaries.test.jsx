fab04159211030b7e02fd6a3b1ea469f715d4ab6
Vitnija Bluzma <thepunkyone@gmail.com>
2019-04-22 18:11:21 +0100

    updated ForecastSummaries component tests according to the walkthrough

--- __tests__/components/forecast-summaries.test.jsx
+++ __tests__/components/forecast-summaries.test.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import ForecastSummaries from '../../src/components/forecast-summaries';
+import ForecastSummary from '../../src/components/forecast-summary';
+
 
 describe('ForecastSummaries component', () => {
   let wrapper;
@@ -10,14 +12,18 @@ describe('ForecastSummaries component', () => {
       date: 1234,
       description: 'description',
       icon: '10',
-      temperature: 20,
+      temperature: {
+        max: 20,
+      },
     },
     {
       key: 5678,
       date: 5678,
       description: 'description2',
       icon: '12',
-      temperature: 27,
+      temperature: {
+        max: 27,
+      },
     },
   ];
   beforeEach(() => {
@@ -30,8 +36,18 @@ describe('ForecastSummaries component', () => {
 
   it('renders an array of ForecastSummary components', () => {
     expect(wrapper.find('ForecastSummary')).toHaveLength(2);
+  });
+
+  it('ForecastSummary components get passed the correct props', () => {
+    wrapper.find('ForecastSummary').forEach(summary => {
+      expect(summary.prop('key')).toBeTruthy;
+      expect(summary.prop('date')).toBeTruthy;
+      expect(summary.prop('description')).toBeTruthy;
+      expect(summary.prop('icon')).toBeTruthy;
+      expect(summary.prop('temperature.max')).toBeTruthy;
+    });
+
     expect(wrapper.find('ForecastSummary').at(0).prop('date')).toBe(1234);
     expect(wrapper.find('ForecastSummary').at(1).prop('icon')).toBe('12');
   });
-
 });
