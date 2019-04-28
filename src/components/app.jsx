b169003061f85986af434f87fa763c8cfe6c84bf
Vitnija Bluzma <thepunkyone@gmail.com>
2019-04-28 15:51:51 +0100

    added setState method to componentDidMount

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import LocationDetails from '../components/location-details';
+import SearchForm from '../components/search-form';
 import ForecastSummaries from '../components/forecast-summaries';
 import ForecastDetails from '../components/forecast-details';
 import '../styles/app.scss';
@@ -15,8 +16,23 @@ class App extends React.Component {
         city: '',
         country: '',
       },
+      error: false,
     };
     this.handleForecastSelect = this.handleForecastSelect.bind(this);
+  }
+
+  componentDidMount() {
+    const url = 'https://mcr-codes-weather.herokuapp.com/forecast?city=Manchester';
+    axios.get(url).then(response => {
+      const data = response.data;
+      this.setState({
+        forecasts: data.forecasts,
+        location: {
+          city: data.location.city,
+          country: data.location.country,
+        },
+      });
+    });
   }
 
   handleForecastSelect(date) {
@@ -37,6 +53,7 @@ class App extends React.Component {
           city={state.location.city}
           country={state.location.country}
         />
+        <SearchForm />
         <ForecastSummaries forecasts={state.forecasts} onForecastSelect={this.handleForecastSelect} />
         {
           selectedForecast && <ForecastDetails forecast={selectedForecast} />
